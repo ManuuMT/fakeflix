7abb98cd7063e7392b04b04cac3c44f3bebcaba9
Emanuel Maldonado <ema14mt@gmail.com>
2023-01-11 12:43:17 -0300

    Hero styles added

--- src/pages/Home/components/Hero/Hero.tsx
+++ src/pages/Home/components/Hero/Hero.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./styles/Hero.scss";
 import axios from "axios";
+import iconReload from "../../../../assets/icons/iconReload.png";
+import iconMute from "../../../../assets/icons/iconMute.png";
+import iconUnmute from "../../../../assets/icons/iconUnmute.png";
+import robotLogo from "../../../../assets/mrRobotLogo.png";
+import iconArrow from "../../../../assets/icons/iconArrow.png";
+import iconInfo from "../../../../assets/icons/iconInfo.png";
 
 export interface HeroInterface {}
 
@@ -37,6 +43,23 @@ const Hero: React.FC<HeroInterface> = () => {
 
   return (
     <div className="hero">
+      <img src={robotLogo} alt="robot" className="hero-title" />
+      <div className="hero-description">
+        <p>
+          A contemporary and culturally resonant drama about a young programmer,
+          Elliot, who suffers from a debilitating anti-social disorder and
+          decides that he can only connect to people by hacking them. He wields
+          his skills as a weapon to protect the people that he cares about.
+        </p>
+      </div>
+      <button className="hero-button-play">
+        <img src={iconArrow} alt="play" />
+        Reproducir
+      </button>
+      <button className="hero-button-info">
+        <img src={iconInfo} alt="info" />
+        Más información
+      </button>
       {videoEnded ? (
         <img className="hero-poster" src={posterUrl} alt="robot" />
       ) : (
@@ -49,8 +72,16 @@ const Hero: React.FC<HeroInterface> = () => {
           Sorry, your browser doesn't support embedded videos.
         </video>
       )}
-      <div className="hero-title">{heroData?.name}</div>
-      <button onClick={() => setMuted(false)}>MUTE OFF</button>
+      {videoEnded ? (
+        <button className="hero-button" onClick={() => setVideoEnded(false)}>
+          <img src={iconReload} alt="reload" />
+        </button>
+      ) : (
+        <button className="hero-button" onClick={() => setMuted(!muted)}>
+          <img src={muted ? iconMute : iconUnmute} alt="volume" />
+        </button>
+      )}
+      <div className="hero-clasif">13+</div>
     </div>
   );
 };
