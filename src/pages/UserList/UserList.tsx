958a70066350ae97b47c9dd8fd2f7641a646c003
Emanuel Maldonado <ema14mt@gmail.com>
2023-01-18 04:54:06 -0300

    Add User feature finished

--- src/pages/UserList/UserList.tsx
+++ src/pages/UserList/UserList.tsx
@@ -1,35 +1,21 @@
 import React, { Fragment, useState } from "react";
-import { ProfileCard } from "./components/ProfileCard";
-import { SplashScreen } from "./components/SplashScreen";
-import "./UserList.scss";
-import { Profile } from "./UserList+Helper";
+import iconPlus from "../../assets/icons/iconPlus.png";
+import Profile5 from "../../assets/profile-icons/profile-05.png";
 import { useAppDispatch, useAppSelector } from "../../redux/states/hooks";
 import { modifyUser, selectUsers } from "../../redux/states/users.state";
-import iconPlus from "../../assets/icons/iconPlus.png";
-import { createUser } from "../../redux/states/users.state";
-import Profile5 from "../../assets/profile-icons/profile-05.png";
 import { Modal } from "./components/Modal";
+import { ProfileCard } from "./components/ProfileCard";
+import { SplashScreen } from "./components/SplashScreen";
+import { Profile } from "./UserList+Helper";
+import "./UserList.scss";
 
 const UserList: React.FC = () => {
   // * States
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [openModal, setOpenModal] = useState(false);
 
   const stateUsers = useAppSelector(selectUsers);
   const dispatch = useAppDispatch();
-
-  const AddUser = () => {
-    dispatch(
-      createUser({
-        id: 1,
-        name: "New User",
-        icon: {
-          src: Profile5,
-          alt: "Profile5",
-        },
-      })
-    );
-  };
 
   const ModifyUser = () => {
     dispatch(
@@ -50,36 +36,43 @@ const UserList: React.FC = () => {
         <SplashScreen videoEnded={setLoading} />
       ) : (
         <Fragment>
-          {openModal && <Modal />}
-          <div className="users">
-            <div className="users-main">
-              <div className="users-title">
-                <h1>Who's watching?</h1>
-              </div>
-              <div className="users-list">
-                {stateUsers.map((user: Profile, i) => (
-                  <ProfileCard profile={user} key={i} />
-                ))}
-                <div
-                  className="profile-card"
+          {openModal ? (
+            <Modal closeModal={() => setOpenModal(false)} />
+          ) : (
+            <div className="users">
+              <div className="users-main">
+                <div className="users-title">
+                  <h1>Who's watching?</h1>
+                </div>
+                <div className="users-list">
+                  {stateUsers.map((user: Profile, i) => (
+                    <ProfileCard profile={user} key={i} />
+                  ))}
+                  <div
+                    className="profile-card"
+                    onClick={() => setOpenModal(true)}
+                  >
+                    <img
+                      className="profile-card-img"
+                      src={iconPlus}
+                      alt="add"
+                    />
+                    <div className="profile-card-name">Add Profile</div>
+                  </div>
+                </div>
+
+                <button
+                  className="users-admin"
                   onClick={() => setOpenModal(true)}
                 >
-                  <img className="profile-card-img" src={iconPlus} alt="add" />
-                  <div className="profile-card-name">Add Profile</div>
-                </div>
+                  Manage profiles
+                </button>
+                <button className="users-admin" onClick={ModifyUser}>
+                  Modify user 1
+                </button>
               </div>
-
-              <button
-                className="users-admin"
-                onClick={() => setOpenModal(true)}
-              >
-                Manage profiles
-              </button>
-              <button className="users-admin" onClick={ModifyUser}>
-                Modify user 1
-              </button>
             </div>
-          </div>
+          )}
         </Fragment>
       )}
     </Fragment>
--- src/pages/UserList/components/Modal/Modal.tsx
+++ src/pages/UserList/components/Modal/Modal.tsx
@@ -1,27 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Modal.scss";
 import Profile5 from "../../../../assets/profile-icons/profile-05.png";
+import { Link } from "react-router-dom";
+import { useAppDispatch } from "../../../../redux";
+import { createUser } from "../../../../redux/states/users.state";
 
-export interface ModalInterface {}
+export interface ModalInterface {
+  closeModal: () => void;
+}
 
-const Modal: React.FC<ModalInterface> = () => {
+const Modal: React.FC<ModalInterface> = (props) => {
+  // * States
+  const [value, setValue] = useState("");
+  const dispatch = useAppDispatch();
+
+  // * Methods
+  const CloseModal = () => props.closeModal();
+
+  const AddUser = () => {
+    dispatch(
+      createUser({
+        id: 6,
+        name: value || "John Doe",
+        icon: {
+          src: Profile5,
+          alt: "Profile5",
+        },
+      })
+    );
+    CloseModal();
+  };
   return (
     <div className="add-modal">
       <div className="add-modal-container">
         <div className="add-modal-content">
           <h1 className="add-modal-title">Add Profile</h1>
           <h3 className="add-modal-subtitle">
-            Add a profile for another person watchnig Netflix.
+            Add a profile for another person watching Netflix.
           </h3>
           <div className="add-modal-divider" />
           <div className="add-modal-main">
             <img className="add-modal-img" src={Profile5} alt="Add Profile" />
-            <input className="add-modal-input" placeholder="Name" type="text" />
+            <input
+              value={value}
+              autoFocus
+              onChange={(e) => setValue(e.target.value)}
+              className="add-modal-input"
+              placeholder="Name"
+              type="text"
+            />
           </div>
           <div className="add-modal-divider" />
           <div className="add-modal-buttons">
-            <div className="add-modal-button-ok">Continue</div>
-            <div className="add-modal-button-cancel">Cancel</div>
+            <Link to="/">
+              <div className="add-modal-button-ok" onClick={AddUser}>
+                Continue
+              </div>
+            </Link>
+            <Link to="/">
+              <div className="add-modal-button-cancel" onClick={CloseModal}>
+                Cancel
+              </div>
+            </Link>
           </div>
         </div>
       </div>
